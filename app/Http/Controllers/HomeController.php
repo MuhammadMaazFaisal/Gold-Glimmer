03cc52d7a6e58a9b5666c8e7f43adb3cb01d4404
Muhammad Maaz <m.maazfaisal0301@gmail.com>
2024-04-24 11:55:36 +0500

    fixed migrations and dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Customer;
+use App\Models\FinishedProduct;
+use App\Models\Order;
+use App\Models\PosOrder;
+use App\Models\Product;
+use App\Models\Vendor;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard');
+        $finishedProducts = FinishedProduct::count();
+        $unfinishedProducts =Product::where('status', 0)->count();
+        $vendors= Vendor::count();
+        $posOrders = PosOrder::count();
+        $customers = Customer::count();
+        $orders=Order::count();
+        return view('admin.dashboard', compact('finishedProducts', 'unfinishedProducts', 'vendors', 'posOrders', 'customers', 'orders'));
     }
 }
